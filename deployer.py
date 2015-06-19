3a60cb1dcc9a1b30c29c1f76d19f8e741631b9f0
Jonathan Apodaca <jonathan.apodaca@colostate.edu>
2015-06-19 16:03:11 -0600

    Updated deployer plugin

--- deployer.py
+++ deployer.py
@@ -1,4 +1,5 @@
 import sublime, sublime_plugin, subprocess, os.path
+from functools import partial
 
 class DeployerRsyncShowStatusCommand(sublime_plugin.TextCommand):
 	def run(self, edit, status):
@@ -13,102 +14,84 @@ class DeployerRsyncDeployCommand(sublime_plugin.TextCommand):
 		newView = self.view.window().new_file()
 		newView.run_command('deployer_rsync_show_status', { 'status': text })
 
-	def shell_cmd(self, shell):
+	def shell_cmd(self, parameters):
 		''' Run the given shell command using bash, and the users' environment '''
-		
 		directoryName = os.path.dirname(self.view.window().project_file_name()) + '/'
-		cmd = subprocess.Popen('source ~/.profile && ' + shell, shell=True, executable='/bin/bash', cwd=directoryName, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+		cmd = subprocess.Popen(parameters, shell=True, executable='/bin/bash', cwd=directoryName, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 		out, err = cmd.communicate()
 		return (cmd.returncode, out.decode('utf-8'), err.decode('utf-8'))
 
-	def __publishTo(self, deployTarget, clean, dryRun):
-		''' Run rsync and pre-build command (if defined) '''
-
-		print('Publishing to ' + deployTarget['name'])
-		print('Clean?', clean)
-		print('Dry-Run?', dryRun)
-
-		rsync = self.view.settings().get('rsync')
-		directoryName = os.path.dirname(self.view.window().project_file_name()) + '/'
-
-		#
-		# BEGIN build rsync command
-		#
-		cmd = [ 'rsync' ]
-		if dryRun:
-			cmd.append('-avn')
-		else:
-			cmd.append('-av')
-
-		if clean:
-			cmd.append('--delete')
-
-		if 'source_directory' in rsync:
-			cmd.append(directoryName + rsync['source_directory'])
-		else:
-			cmd.append(directoryName + '.build/')
-
-		cmd.append(deployTarget['dest'])
-		cmd = ' '.join(cmd)
-		#
-		# END build rsync command
-		#
-
-		print(cmd)
-
-		sublime.status_message('Running rsync')
-		(stat, out, err) = self.shell_cmd(cmd)
-		self.display_text('STDOUT:\n' + out + '\n\nSTDERR:\n' + err)
-
 	def run(self, edit):
-		print('deployer_rsync_deploy')
-
-		#
-		# BEGIN validation
-		#
-		if self.view.window().project_file_name() == None:
-			sublime.status_message('Could not open project file.')
+		if self.view.window().project_file_name() is None:
+			sublime.status_message('You cannot run this command in non-project contexts')
 			return
 
 		s = self.view.settings()
-		if not self.view.settings().has('rsync'):
-			sublime.status_message('There are no rsync options defined in settings.')
+		if not s.has('rsync'):
+			sublime.status_message('No option "rsync" found')
+			return
+		rsync = s.get('rsync')
+
+		if not 'destinations' in rsync:
+			sublime.status_message('No option "rsync.destinations" found')
+			return
+		destinations = rsync['destinations']
+
+		if not isinstance(destinations, list):
+			sublime.status_message('The option "rsync.destinations" is not a list')
 			return
 
-		rsync = self.view.settings().get('rsync')
-		if not 'deploy_targets' in rsync:
-			sublime.status_message('Sub-option "deploy_targets" not found in "rsync".')
-			return
-
-		deployTargets = rsync['deploy_targets']
-		valid = all(map(lambda target: 'name' in target and 'dest' in target, deployTargets))
+		valid = all(map(lambda x: isinstance(x, dict) and 'name' in x and 'destination' in x, destinations))
 		if not valid:
-			sublime.status_message('Some (or all) deploy_targets do not have all required attributes "name" and "dest".')
+			sublime.status_message('At least one entry in "rsync.destinations" does not have a "name" or "destination" property.')
 			return
-		#
-		# END validation
-		#
 
-		deployNames = list(map(lambda target: target['name'], deployTargets))
-		self.view.window().show_quick_panel(deployNames, self.deploy_target_selected, sublime.MONOSPACE_FONT, 0)
+		#
+		# Show destination selector:
+		#
+		self.view.window().show_quick_panel(
+			list(map(lambda x: x['name'], destinations)),
+			self.deploy_target_selected,
+			sublime.MONOSPACE_FONT,
+			0)
 
 	def deploy_target_selected(self, index):
 		if index == -1:
 			return
 
-		deployTarget = self.view.settings().get('rsync')['deploy_targets'][index]
-		def which_publish(i):
-			clean = True if i == 2 or i == 3 else False
-			dryRun = True if i == 1 or i == 3 else False
-			def confirm(i):
-				if i == 1:
-					self.__publishTo(deployTarget, clean, dryRun)
+		dest = self.view.settings().get('rsync')['destinations'][index]			
+		self.view.window().show_quick_panel([ 'Deploy (--dry-run)', 'Deploy (normal)', 'Deploy (--dry-run --delete)', 'Deploy (--delete)' ], partial(self.type_selected, dest), sublime.MONOSPACE_FONT, 0)
+	
+	def type_selected(self, dest, index):
+		if index == -1:
+			return
 
-			self.view.window().show_quick_panel([ 'Cancel', 'Continue' ], confirm, sublime.MONOSPACE_FONT, 0)
+		cmd = []
+		if index == 0:
+			cmd = [ 'rsync', '-avn' ] # --dry-run
+		elif index == 1:
+			cmd = [ 'rsync', '-av' ] # normal
+		elif index == 2:
+			cmd = [ 'rsync', '-avn', '--delete' ] # --dry-run --delete
+		elif index == 3:
+			cmd = [ 'rsync', '-av', '--delete' ] # --delete
 
-		self.view.window().show_quick_panel([
-			'Publish to ' + deployTarget['name'],
-			'Publish to ' + deployTarget['name'] + ' (dry-run)',
-			'Clean Publish to ' + deployTarget['name'],
-			'Clean Publish to ' + deployTarget['name'] + ' (dry-run)'
-		], which_publish, sublime.MONOSPACE_FONT, 0)
+		self.view.window().show_quick_panel([ 'Commit' ], partial(self.commit_selected, dest, cmd), sublime.MONOSPACE_FONT, 0)
+
+	def commit_selected(self, dest, cmd, index):
+		if index == -1:
+			return
+
+		rsync = self.view.settings().get('rsync')
+		if 'exclude' in rsync:
+			x = rsync['exclude']
+			if isinstance(x, list):
+				for exclude in x:
+					cmd.extend([ '--exclude', '"' + exclude + '"' ])
+
+		cmd.extend([ './', dest['destination'] ])
+		sublime.status_message('doing it...')
+		print(cmd)
+		(code, out, err) = self.shell_cmd(' '.join(cmd))
+
+		self.display_text('R-SYNC COMMAND:\n' + ' '.join(cmd) + '\n\nSTDOUT:\n' + out + '\nSTDERR:\n' + err)
